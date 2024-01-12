a71e50ae7daa5083c91cfe22ed56d915356ad877
MThilakshana <madurathilakshana999@gmail.com>
2024-01-12 08:53:27 +0530

    12/01

---
+++ startCls.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+root=Tk()
+root.title('Start Lecture - LearnMaster 1.0')
+root.geometry('775x400+300+200')
+root.configure(bg="#fff")
+root.resizable(False,False)
+
+
+
+root.mainloop()
