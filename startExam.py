e682fe21101b909cfab2d5962173bb286e0de252
MThilakshana <madurathilakshana999@gmail.com>
2024-01-26 08:23:28 +0530

    26/01

--- startExam.py
+++ startExam.py
@@ -23,18 +23,52 @@ def checkBtn():
         sql = "SELECT Name,Time,Date,SID FROM examdetails WHERE EID = %s"
         record = (eid.get(),)
         cursor.execute(sql,record)
-        Name = cursor.fetchone()
+        data = cursor.fetchone()
         
-        if(Name==None):
+        if(data==None):
             messagebox.showinfo("Warning","Invalid Exam ID!")
         else:
-            examnameEntry.insert(0,Name[0])
-            
-    
+            examnameEntry.insert(0,data[0])
+            timeEntry.insert(0,data[1])
+            dateEntry.insert(0,data[2])
+            sidentry.insert(0,data[3])
+
     
 #start exam button 
 def startExambtn():
-    x= 2
+    exm_id = eid.get()
+    exm_name = examnameEntry.get()
+    exm_subject = sidentry.get()
+    exm_date = str(dateEntry.get())
+    exm_time = timeEntry.get()
+    
+    
+    #connect to the Firebase
+    config = {
+            "apiKey": "AIzaSyCEu0-KtmUoM6ilvpIYy6vidHnVs93aO78",
+            "authDomain": "edumaster-project.firebaseapp.com",
+            "projectId": "edumaster-project",
+            "databaseURL": "https://edumaster-project-default-rtdb.firebaseio.com/",
+            "storageBucket": "edumaster-project.appspot.com",
+            "messagingSenderId": "945743272123",
+            "appId": "1:945743272123:web:e64de0b72d8b7e6e26f19e",
+            "measurementId": "G-XNK127X4XJ"
+    }
+      
+    firebase = pyrebase.initialize_app(config)
+    database = firebase.database()
+    
+    savedata = {"Exam ID":exm_id,"Exam Name":exm_name,"Subject":exm_subject,"Date":exm_date,"Time":exm_time}
+    
+    path = exm_id+"_"+exm_subject
+    #save data
+    response = database.child("Exam").child(path).set(savedata)
+    
+    messagebox.showinfo("Message","Exam Ready to Start!")
+    cursor.close()
+    root.destroy()
+    
+      
 
 root=Tk()
 root.title('Start Lecture - LearnMaster 1.0')
@@ -110,20 +144,20 @@ Frame(root,
       height=2,
       bg='black').place(x=145,y=175)
 
-#No of students
+#student id
 subidlb = Label(root,
                   text="Subject ID",
                   font=('Microsoft YaHei UI Light',11),
                   bg="white",
                   fg='black').place(x=25,y=215)
 
-sid = Entry(root,
+sidentry = Entry(root,
                     width=30,
                     fg='Black',
                     border=0,
                     bg='White',
                     font=('Microsoft YaHei UI Light',11))
-sid.place(x=150,y=215)
+sidentry.place(x=150,y=215)
 
 Frame(root,
       width=260,
@@ -131,7 +165,7 @@ Frame(root,
       bg='black').place(x=145,y=245)
 
 #Date
-clsdatelb = Label(root,
+exmdatelb = Label(root,
                   text="Date",
                   font=('Microsoft YaHei UI Light',11),
                   bg="white",
@@ -151,7 +185,7 @@ Frame(root,
       bg='black').place(x=145,y=315)
 
 #time
-clstimelb = Label(root,
+exmtimelb = Label(root,
                   text="Time",
                   font=('Microsoft YaHei UI Light',11),
                   bg="white",
