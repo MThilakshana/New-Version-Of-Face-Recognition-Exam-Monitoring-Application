3bd14db2bfb1132f9b321f927de7e5f8ecef224b
MThilakshana <madurathilakshana999@gmail.com>
2025-02-19 21:01:34 +0530

    02/19

--- ResultWindow.py
+++ ResultWindow.py
@@ -1,72 +1,69 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+import pyrebase
 
-def getinput():
+def addDataToTreeView():
+      examid = exid_entry.get()
+      stuid = sid_entry.get()
       
-      def displayrep():
-            stuid = userid_entry.get()
-            examid = examid_entry.get()
+      data = database.child('AuthorizedNotDetected').child(examid).get()
+      data2 = database.child('AuthorizedNotDetected').child(examid).child(stuid).get()
+      
+      length = len(data2.val())
+      tab_data = {}
+      print(length)
+      if (length>3):
+            length = length - 3
+            for i in range(length):
+                  datatab = data.val()
+                  tab_data[stuid] = datatab
             
-            if(stuid=="" or examid==""):
-                  messagebox.showinfo("Warning","All fields required!")
-            else:
-                  return examid,stuid
+            count = 0
+            for stdi,details in tab_data.items():
+                  id = count + 1
+                  datavv = "Autorized preson not detect"
+                  val = datavv + " time "+str(id)
+                  st_data = "Authorized person not detected"
+                  time = tab_data.get("Autorized preson not detect time 1")
+                  print(time)
+                  my_tree.insert("","end",values=(id,st_data,time))
+                  count = count + 1
+      else:
+            print('skipped the if condition')
+                  
+#for det in data.each():
             
-      window = Tk()
-      window.title('Exam Report - Get Info')
-      window.geometry('250x200')
-      window.configure(bg="#fff")
-      window.resizable(False,False)
+def loaddata():
+      examid = exid_entry.get()
+      stuid = sid_entry.get()
       
-      exam_id = Label(window,
-                  text="Exam ID",
-                  font=('Microsoft YaHei UI Light',11),
-                  bg="white",
-                  fg='black').place(x=15,y=20)
+      if examid=="" or stuid=="":
+            messagebox.showinfo("Warning","All field required!")
+      else:
+            details = database.child("Finished_Exam").child(examid).child(stuid).get().val()
       
-      examid_entry = Entry(window,
-                    width=25,
-                    fg='Black',
-                    border=1,
-                    bg='White',
-                    font=('Microsoft YaHei UI Light',11))
-      examid_entry.place(x=15,y=50)
+            if details:
+                  totscreentime_entry.insert(0,details.get('Total Screen Time'))
+                  authscreentime_entry.insert(0,details.get('Total Authorized time'))
+                  unauthscreentime_entry.insert(0,details.get('Total Unauthorized Time'))
+            addDataToTreeView()
       
-      userid = Label(window,
-                  text="Student ID",
-                  font=('Microsoft YaHei UI Light',11),
-                  bg="white",
-                  fg='black').place(x=15,y=80)
-      
-      userid_entry = Entry(window,
-                    width=25,
-                    fg='Black',
-                    border=1,
-                    bg='White',
-                    font=('Microsoft YaHei UI Light',11))
-      userid_entry.place(x=15,y=110)
-      
-      startbtn = Button(window,
-                text="Enter",
-                bg="#57a1f8",
-                fg="white",
-                width=10,
-                height=0,
-                pady=2,
-                cursor='hand2',
-                border=0,
-                font=('Microsoft YaHei UI Light',11, 'bold'),
-                command=displayrep)
-      startbtn.place(x=15,y=140)
 
-      window.mainloop()
-
-examid,stuid = getinput()
-
-def showdata():
-      return None
-
+config = {
+      "apiKey": "AIzaSyCEu0-KtmUoM6ilvpIYy6vidHnVs93aO78",
+      "authDomain": "edumaster-project.firebaseapp.com",
+      "projectId": "edumaster-project",
+      "databaseURL": "https://edumaster-project-default-rtdb.firebaseio.com/",
+      "storageBucket": "edumaster-project.appspot.com",
+      "messagingSenderId": "945743272123",
+      "appId": "1:945743272123:web:e64de0b72d8b7e6e26f19e",
+      "measurementId": "G-XNK127X4XJ"
+}
+        
+firebase = pyrebase.initialize_app(config)
+database = firebase.database()
+        
 root=Tk()
 root.title('Exam Report - LearnMaster 2.0')
 root.geometry('700x500+300+200')
@@ -112,6 +109,18 @@ unauthscreentime = Label(sideframe,
                   bg="white",
                   fg='black').place(x=15,y=140)
 
+exid = Label(sideframe,
+                  text="EXAM ID",
+                  font=('Microsoft YaHei UI Light',11),
+                  bg="white",
+                  fg='black').place(x=15,y=220)
+
+sid = Label(sideframe,
+                  text="STUDENT ID",
+                  font=('Microsoft YaHei UI Light',11),
+                  bg="white",
+                  fg='black').place(x=15,y=280)
+
 
 #add entry box
 totscreentime_entry = Entry(sideframe,
@@ -138,6 +147,22 @@ unauthscreentime_entry = Entry(sideframe,
                     font=('Microsoft YaHei UI Light',11))
 unauthscreentime_entry.place(x=15,y=170)
 
+exid_entry = Entry(sideframe,
+                    width=25,
+                    fg='Black',
+                    border=1,
+                    bg='White',
+                    font=('Microsoft YaHei UI Light',11))
+exid_entry.place(x=15,y=250)
+
+sid_entry = Entry(sideframe,
+                    width=25,
+                    fg='Black',
+                    border=1,
+                    bg='White',
+                    font=('Microsoft YaHei UI Light',11))
+sid_entry.place(x=15,y=310)
+
 #add button
 exitbtn = Button(sideframe,
                 text="Exit",
@@ -151,6 +176,19 @@ exitbtn = Button(sideframe,
                 font=('Microsoft YaHei UI Light',8),
                 command=root.destroy)
 exitbtn.place(x=85,y=380)
+
+startbtn = Button(sideframe,
+                text="Load Data",
+                bg="#57a1f8",
+                fg="white",
+                width=10,
+                height=0,
+                pady=2,
+                cursor='hand2',
+                border=0,
+                font=('Microsoft YaHei UI Light',11, 'bold'),
+                command=loaddata)
+startbtn.place(x=70,y=350)
 
 #add result table
 my_tree = ttk.Treeview(root,height=18)
@@ -173,5 +211,7 @@ my_tree.heading('reason',text='Reason',anchor=CENTER)
 my_tree.heading('date',text='Date and Time',anchor=CENTER)
 
 my_tree.place(x=10,y=100)
+
+#showdata()
 
 root.mainloop()
